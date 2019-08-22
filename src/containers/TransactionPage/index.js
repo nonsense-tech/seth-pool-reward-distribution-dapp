6bec2eedb21627c4ccdb8d19557571da491d2ee8
Max Alekseenko <nnwen0609@gmail.com>
2019-08-23 01:19:14 +0300

    create transaction with data from csv

--- src/containers/TransactionPage/index.js
+++ src/containers/TransactionPage/index.js
@@ -3,19 +3,29 @@ import { connect } from 'react-redux';
 
 import { create } from '../../store/multisig/actions';
 
+const defaultRecipientsData = `0x79DF43B54c31c72a3d93465bdf72317C751822B3,1000000000000000000\n0x9249fE90Ed0782729532140a2Da4814b5e6C46f1,2000000000000000000`;
 class TransactionPage extends Component {
+  state = {
+    recipientsData: defaultRecipientsData,
+  }
+  onRecipientsDataChange = e => {
+    this.setState({
+      recipientsData: e.target.value,
+    });
+  }
   render() {
     const { owners, account, history, create } = this.props;
     const isOwner = owners.includes(account);
     if (!isOwner) {
       history.push('/');
     }
+    
     return (
       <div>
-        <textarea style={{ width: 500, height: 200 }}></textarea>
+        <textarea style={{ width: 500, height: 200 }} value={this.state.recipientsData} onChange={this.onRecipientsDataChange}></textarea>
         <br />
         <br />
-        <button onClick={create}>Send transaction</button>
+        <button onClick={() => create(this.state.recipientsData)}>Send transaction</button>
       </div>
     );
   }
--- src/store/multisig/actions.js
+++ src/store/multisig/actions.js
@@ -1,10 +1,14 @@
+import papaparse from 'papaparse';
+
 import { INITIALIZE } from './constants';
-import Multisig from '../../contracts/multisig.json';
+import MultisigABI from '../../contracts/ABIs/multisig.json';
+import AirdropperABI from '../../contracts/ABIs/airdropper.json';
+import addresses from '../../contracts/addresses.json';
 
 export function initialize() {
   return async (dispatch, getState) => {
     const web3 = getState().web3connect.web3;
-    const instance = new web3.eth.Contract(Multisig.abi, Multisig.address);
+    const instance = new web3.eth.Contract(MultisigABI, addresses.multisig);
     const owners = await instance.methods.getOwners().call();
 
     const transactionCount = Number(await instance.methods.transactionCount().call());
@@ -22,11 +26,21 @@ export function initialize() {
   };
 }
 
-export function create() {
+export function create(recipientsData) {
   return async (dispatch, getState) => {
     const instance = getState().multisig.instance;
-    const account = getState().web3connect.account;
-    await instance.methods.submitTransaction(account, '1', '0x').send({ from: account });
+    const { web3, account } = getState().web3connect;
+    const airdropper = new web3.eth.Contract(AirdropperABI, addresses.airdropper);
+
+    const parsedData = papaparse.parse(recipientsData, { delimiter: ',', header: false, skipEmptyLines: true }).data;
+    const recipientsAddresses = [];
+    const recipientsShares = [];
+    parsedData.forEach(item => {
+      recipientsAddresses.push(item[0]);
+      recipientsShares.push(item[1]);
+    });
+    const transactionData = airdropper.methods.multisend(addresses.token, recipientsAddresses, recipientsShares).encodeABI();
+    await instance.methods.submitTransaction(addresses.airdropper, 0, transactionData).send({ from: account });
   };
 }
 
